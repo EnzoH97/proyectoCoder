fbbc943de7cb4deaa20fdfd88e3dfea6ee2b0278
enzo herrera <enzoarielherreraortega97@gmail.com>
2025-12-18 21:26:28 -0300

    Se agrego funcionalidad al boton finalizar compra

--- js/carrito.js
+++ js/carrito.js
@@ -3,13 +3,26 @@ let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 const lista = document.getElementById("lista");
 
 function agregarAlCarrito(producto) {
-    carrito.push(producto);
+    const item = carrito.find(p => p.id === producto.id);
+    if (item) {
+        item.cantidad++;
+    } else {
+        carrito.push({
+            ...producto,
+            cantidad: 1
+        })
+    }
     localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
+function finalizarCompra() {
+    localStorage.removeItem("carrito");
+    renderizarCarrito();
+    alert("Gracias por su compra!");
+}
 
 function renderizarCarrito() {
-//actualizacion futura: evitar duplicados y sumar cantidades
+    lista.innerHTML = "";
     let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
     for (let producto of carrito) {
@@ -20,12 +33,17 @@ function renderizarCarrito() {
             <img src="${producto.imagen}" alt="${producto.descripcion}" class="item-imagen">
             <div class="item-info">
                 <p class="item-nombre">${producto.nombre}</p>
-                <p class="item-precio">$${producto.precio}</p>
+                <p class="item-cantidad">Cantidad: ${producto.cantidad}</p>
+                <p class="item-precio">$${producto.precio * producto.cantidad}</p>
             </div>
         `;
 
         lista.appendChild(item);
     }
+
+const botonFinalizar = document.querySelector(".btn-finalizar");
+botonFinalizar.addEventListener("click", finalizarCompra);
+
 //actualizacion futura calcular total a pagar y darle funcionalidad al boton finalizar compra. 
 }
 
